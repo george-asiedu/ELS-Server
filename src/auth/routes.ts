b236f59f77c35f41180be3361564221b78b95565
george-asiedu <asiedug41@gmail.com>
2025-12-17 11:36:16 +0000

    added forgot and reset password routes

--- src/auth/routes.ts
+++ src/auth/routes.ts
@@ -5,5 +5,7 @@ const router: Router = Router();
 
 router.post("/signup", AuthController.signup);
 router.post("/login", AuthController.login);
+router.post("/forgot-password", AuthController.forgotPassword);
+router.post("/reset-password/:token", AuthController.resetPassword);
 
 export default router;
