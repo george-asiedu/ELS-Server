6c7fefb0435118f7e1aa8ae6932cc3bcb5a53a1f
George Asiedu <asiedug41@gmail.com>
2026-09-24 16:17:51 +0000

    Update routes.ts

--- src/auth/routes.ts
+++ src/auth/routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { AuthController } from "./authController";
+import { authenticate } from "../middleware/auth";
 
 const router: Router = Router();
 
 router.post("/signup", AuthController.signup);
 router.post("/login", AuthController.login);
+router.post("/logout", authenticate, AuthController.logout);
 router.post("/forgot-password", AuthController.forgotPassword);
 router.post("/reset-password/:token", AuthController.resetPassword);
 
